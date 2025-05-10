47175da314aed8f23a055cdc906e40a8355ec171
Samuel <85027459+s4m-mo@users.noreply.github.com>
2025-05-10 20:49:32 +0100

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,15 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    const disposable = vscode.commands.registerCommand('extension.start-20-20-20', () => {
-        let interval = setInterval(showMessage, 20 * 60 * 1000);
+    let interval = setInterval(showMessage, 20 * 60 * 1000);
 
-        async function showMessage() {
-            clearInterval(interval);
+    async function showMessage() {
+        clearInterval(interval);
 
-            vscode.window.showInformationMessage('It\'s time to rest your eyes!\n\nLook 20 feet away for 20 seconds.', { modal: true });
+        vscode.window.showInformationMessage('It\'s time to rest your eyes!\n\nLook 20 feet away for 20 seconds.', { modal: true });
 
-            interval = setInterval(showMessage, 20 * 60 * 1000);
-        }
-
-    });
-
-    context.subscriptions.push(disposable);
+        interval = setInterval(showMessage, 20 * 60 * 1000);
+    }
 }
 
 export function deactivate() { }
